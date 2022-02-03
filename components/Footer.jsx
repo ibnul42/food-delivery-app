21cc7906805f10d7eefdb6a783d2909bad0faffc
ibnul42 <ibnulashir42@gmail.com>
2022-02-03 10:35:35 +0600

    fotter design

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,37 @@
+import Image from 'next/image';
+import styles from '../styles/Footer.module.css';
 
 const Footer = () => {
-  return <div>
-      Footer
-  </div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src='/img/bg.png' layout='fill' />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.moto}>
+            We Did it
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h2 className={styles.title}>
+            Find Us
+          </h2>
+          <p className={styles.text}>
+            Don't run
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h2 className={styles.title}>
+            Find Us
+          </h2>
+          <p className={styles.text}>
+            Don't run
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Footer;
